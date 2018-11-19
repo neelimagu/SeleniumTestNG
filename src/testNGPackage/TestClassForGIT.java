1f348822ce4a7827146aad4756b6e2e7620ce8e1
Neelima <neelimagu@gmail.com>
2018-11-19 00:06:32 -0800

    Including TestForGit class

---
+++ src/testNGPackage/TestClassForGIT.java
@@ -0,0 +1,13 @@
+package testNGPackage;
+
+import org.testng.annotations.Test;
+
+public class TestClassForGIT {
+	
+	@Test
+	public void testing(){
+		
+		System.out.println("Test for GIT");
+	}
+
+}
